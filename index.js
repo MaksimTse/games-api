b6a3fc8af003cd3fd6f8cfc1300ab7c6a8d1a132
MaksimTse <maksimplamja@gmail.com>
2024-09-25 11:49:07 +0300

    index got /games directory

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('/games', (req, res) => {
+    res.send(["STALCRAFT:X", "PUBG"])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
